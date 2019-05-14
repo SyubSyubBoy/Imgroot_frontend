fb5231cf99af1159f1a8115a30aaaee8d50f8efa
minchanKim <syubsyubboy@gmail.com>
2019-05-14 10:10:25 +0900

    Add post form

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
     <div class="container">
+      <div class="left-aside">
+        <b-button v-b-modal.modal-post>새 글 추가</b-button>
+        <b-modal id="modal-post" title="새 글 쓰기" hide-footer>
+          <b-form @submit="onSubmit" @reset="onReset">
+            <b-form-group id="form-group-1" label="트리 선택" label-for="form-input-1">
+              <b-form-select id="form-input-1" v-model="form.tree" :options="trees" required></b-form-select>
+            </b-form-group>
+            <b-form-group id="form-group-2" label="제목" label-for="form-input-2">
+              <b-form-input id="form-input-2" v-model="form.title" required></b-form-input>
+            </b-form-group>
+            <b-form-group id="form-group-3" label="내용" label-for="form-input-3">
+              <b-form-textarea id="form-input-3" v-model="form.content" required></b-form-textarea>
+            </b-form-group>
+            <div class="form-buttons">
+              <b-button type="submit" variant="primary">Submit</b-button>
+              <b-button type="reset" variant="danger">Reset</b-button>
+            </div>
+          </b-form>
+        </b-modal>
+      </div>
       <ul class="post-list">
         <li v-for="post in posts" :key="post.postId">
           <PostCard :title="post.postTitle" :content="post.postContent"/>
@@ -11,7 +31,7 @@
 </template>
 
 <script>
-import PostCard from '~/components/PostCard';
+import PostCard from "~/components/PostCard";
 
 export default {
   components: {
@@ -21,11 +41,38 @@ export default {
     this.$store.dispatch("getFeed");
   },
   data() {
-    return {};
+    return {
+      form: {
+        tree: '',
+        title: '',
+        content: '',
+      }
+    };
   },
   computed: {
     posts() {
       return this.$store.state.posts;
+    },
+    trees() {
+      return this.$store.state.myTrees.map(t => {
+        return {
+          value: t.treeId,
+          text: t.treeName
+        }
+      });
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+    },
+    onReset(e) {
+      e.preventDefault();
+      this.form = {
+        tree: '',
+        title: '',
+        content: ''
+      }
     }
   }
 };
@@ -42,9 +89,22 @@ export default {
 
 .post-list {
   list-style-type: none;
+  margin-right: 10vw;
 }
 
 .post-list li {
   margin-top: 2vh;
+}
+
+.left-aside {
+  width: 10vw;
+  margin-top: 2vh;
+  position: sticky;
+  z-index: 1;
+  text-align: left;
+}
+
+.form-buttons {
+  text-align: right;
 }
 </style>
