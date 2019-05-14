bc3063d59f485c68bc19329254200e967c490006
minchanKim <syubsyubboy@gmail.com>
2019-05-14 10:26:19 +0900

    Detach a compontent that features form Add create post request

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,7 @@
   <div>
     <div class="container">
       <div class="left-aside">
-        <b-button v-b-modal.modal-post>새 글 추가</b-button>
-        <b-modal id="modal-post" title="새 글 쓰기" hide-footer>
-          <b-form @submit="onSubmit" @reset="onReset">
-            <b-form-group id="form-group-1" label="트리 선택" label-for="form-input-1">
-              <b-form-select id="form-input-1" v-model="form.tree" :options="trees" required></b-form-select>
-            </b-form-group>
-            <b-form-group id="form-group-2" label="제목" label-for="form-input-2">
-              <b-form-input id="form-input-2" v-model="form.title" required></b-form-input>
-            </b-form-group>
-            <b-form-group id="form-group-3" label="내용" label-for="form-input-3">
-              <b-form-textarea id="form-input-3" v-model="form.content" required></b-form-textarea>
-            </b-form-group>
-            <div class="form-buttons">
-              <b-button type="submit" variant="primary">Submit</b-button>
-              <b-button type="reset" variant="danger">Reset</b-button>
-            </div>
-          </b-form>
-        </b-modal>
+        <PostForm />
       </div>
       <ul class="post-list">
         <li v-for="post in posts" :key="post.postId">
@@ -32,48 +15,19 @@
 
 <script>
 import PostCard from "~/components/PostCard";
+import PostForm from '~/components/PostForm';
 
 export default {
   components: {
-    PostCard
+    PostCard, PostForm
   },
   created() {
     this.$store.dispatch("getFeed");
-  },
-  data() {
-    return {
-      form: {
-        tree: '',
-        title: '',
-        content: '',
-      }
-    };
   },
   computed: {
     posts() {
       return this.$store.state.posts;
     },
-    trees() {
-      return this.$store.state.myTrees.map(t => {
-        return {
-          value: t.treeId,
-          text: t.treeName
-        }
-      });
-    }
-  },
-  methods: {
-    onSubmit(e) {
-      e.preventDefault();
-    },
-    onReset(e) {
-      e.preventDefault();
-      this.form = {
-        tree: '',
-        title: '',
-        content: ''
-      }
-    }
   }
 };
 </script>
